2428a5e5803a8f431ea6b0d5ea798f775f34e8ad
unknown <i-zhutongtong@corp.qihoo.net>
2019-08-04 17:28:14 +0800

    judge isOnline

--- web/js/blog.js
+++ web/js/blog.js
@@ -2,15 +2,26 @@
 	"use strict";
 
 	var offlineIcon;
-	var isLoggedIn = /isLoggedIn=1/.test(document.cookie.toString() || "");
-
-	document.addEventListener("DOMContentLoaded",ready,false);
+	var isLoggedIn = /isLoggedIn=1/.test(document.cookie.toString() || "")
+	var isOnline = "onLine" in navigator && navigator.onLine
+	document.addEventListener("DOMContentLoaded",ready,false)
 
 
 	// **********************************
 
 	function ready() {
 		offlineIcon = document.getElementById("connectivity-status");
+		if(!isOnline) {
+			offlineIcon.classList.remove("hidden")
+		}
+		window.addEventListener("online",function online(){
+			offlineIcon.classList.add("hidden")
+			isOnline = true
+		},false);
+		window.addEventListener("offline",function offline(){
+			offlineIcon.classList.remove("hidden")
+			isOnline = false
+		},false)
 	}
 
 })();
