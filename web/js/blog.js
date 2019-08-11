2e6fe4eda916d5d7c225cf4c8a3cc75a805f25ea
unknown <i-zhutongtong@corp.qihoo.net>
2019-08-11 14:34:07 +0800

    update serviceWorker

--- web/js/blog.js
+++ web/js/blog.js
@@ -4,8 +4,14 @@
 	var offlineIcon;
 	var isLoggedIn = /isLoggedIn=1/.test(document.cookie.toString() || "")
 	var isOnline = "onLine" in navigator && navigator.onLine
+	var usingSW = ("serviceWorker" in navigator)
+	var swRegistration
+	var svWorker
+
+
 	document.addEventListener("DOMContentLoaded",ready,false)
 
+	initServiceWorker().catch(console.error)
 
 	// **********************************
 
@@ -17,11 +23,53 @@
 		window.addEventListener("online",function online(){
 			offlineIcon.classList.add("hidden")
 			isOnline = true
+			sendStatusUpdate()
 		},false);
 		window.addEventListener("offline",function offline(){
 			offlineIcon.classList.remove("hidden")
 			isOnline = false
+			sendStatusUpdate()
 		},false)
+	}
+
+	async function initServiceWorker() {
+		swRegistration = await navigator.serviceWorker.register('/sw.js', {
+			updateViaCache: 'none'
+		})
+		svWorker = swRegistration.installing || swRegistration.waiting || swRegistration.active
+		sendStatusUpdate(svWorker)
+
+		navigator.serviceWorker.addEventListener("controllerchange", function onControl() {
+			svWorker = navigator.serviceWorker.controller
+			sendStatusUpdate(svWorker)
+		})
+		navigator.serviceWorker.addEventListener("message",onSWMessage,false);
+	}
+
+	function onSWMessage(evt) {
+		var { data } = evt;
+		if (data.statusUpdateRequest) {
+			console.log("Status update requested from service worker, responding...");
+			sendStatusUpdate(evt.ports && evt.ports[0]);
+		}
+		else if (data == "force-logout") {
+			document.cookie = "isLoggedIn=";
+			isLoggedIn = false;
+			sendStatusUpdate();
+		}
+	}
+
+	function sendStatusUpdate(target) {
+		sendSWMessage({statusUpdate: {isOnline, isLoggedIn}}, target)
+	}
+	function sendSWMessage(msg, target) {
+		if (target) {
+			target.postMessage(msg)
+		} else if(svWorker) {
+			svWorker.postMessage(msg)
+		} else if(navigator.serviceWorker.controller) {
+			navigator.serviceWorker.controller.postMessage(msg)
+		}
 	}
 
 })();
--- web/js/sw.js
+++ web/js/sw.js
@@ -1,3 +1,46 @@
 "use strict";
 
 // TODO
+const version = 1
+var isOnline = true;
+var isLoggedIn = false;
+self.addEventListener('install', onInstall)
+self.addEventListener('activate', onActive)
+self.addEventListener("message", onMessage);
+
+main().catch(console.error)
+
+async function main() {
+  await sendMessage({ statusUpdateRequest: true });
+  console.log(`service worker ${version} is starting...`)
+}
+
+async function onInstall(e) {
+  console.log(`service worker ${version} is install...`)
+  self.skipWaiting()
+}
+
+function onActive(e) {
+  e.waitUntil(handleActive())
+}
+async function handleActive() {
+  await clients.claim()
+  console.log(`service worker ${version} is active...`)
+}
+
+function onMessage({ data }) {
+	if ("statusUpdate" in data) {
+		({ isOnline, isLoggedIn } = data.statusUpdate);
+		console.log(`Service Worker (v${version}) status update... isOnline:${isOnline}, isLoggedIn:${isLoggedIn}`);
+	}
+}
+async function sendMessage(msg) {
+	var allClients = await clients.matchAll({ includeUncontrolled: true, });
+	return Promise.all(
+		allClients.map(function sendTo(client){
+			var chan = new MessageChannel();
+			chan.port1.onmessage = onMessage;
+			return client.postMessage(msg,[chan.port2]);
+		})
+	);
+}
